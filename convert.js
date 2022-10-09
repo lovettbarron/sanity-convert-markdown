faa59ba884a17018b421f9defd168ac95920cb03
Andrew Lovett Barron <albkeel@albkeel.local>
2022-10-10 01:40:04 +0200

    Sanityh convert

---
+++ convert.js
@@ -0,0 +1,83 @@
+/* eslint-disable */
+const fs = require("fs");
+const sanityClient = require("@sanity/client");
+const groq = require("groq");
+const BlocksToMarkdown = require("@sanity/block-content-to-markdown");
+const config = {
+  projectId: "lze1vo8m",
+  dataset: "andrewlbcom",
+  useCdn: true,
+  apiVersion: "2022-02-03",
+  token: "sanityauthtoken",
+};
+const client = sanityClient(config);
+const projectId = config.projectId;
+const dataset = config.dataset;
+console.log(client.config());
+const imageOptions = {};
+const serializers = {
+  types: {
+    code: (props) => `\`\`\`${props.node.language}\n${props.node.code}\n\`\`\``,
+    codesandbox: ({ node }) =>
+      `<iframe src="https://codesandbox.io/embed/${node.id}?fontsize=14" style="width:100%; height:500px; border:0; border-radius: 4px; overflow:hidden;" sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"></iframe>`,
+    youtube: ({ node }) =>
+      `<iframe width="560" height="315" src="https://www.youtube.com/embed/${node.id}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+  },
+  marks: {
+    internalLink: (props) => `![${props.children[0]}](${props.mark.link})`,
+  },
+};
+
+async function getDocs() {
+  const filter = '*[_type == "article"]';
+  const projection = groq`{
+    _id,
+    _type,
+    "author": author->{name},
+    "thumbnail": thumbnail.asset -> {url},
+    title,
+    blurb,
+    slug,
+    date,
+    body[]{
+      ...,
+      children[]{
+        ...,
+        _type == "muxVideo" => {
+            "playbackId": asset->playbackId
+          },
+        _type == "internalLink" => {
+            "type": @->_type,
+            "slug": @->slug.current,
+        }
+      }
+    }
+  }`;
+  const query = [filter, projection].join(" ");
+  const docs = await client.fetch(query);
+  docs.forEach((post) => {
+    const { title, blurb, author, thumbnail, body, slug, date } = post;
+    const mdslug = slug || title.replace(" ", "-").toLowerCase();
+    const content = `
+---
+layout: post
+title: "${title}"
+date: "${date.split("T")[0]}",
+slug: "${mdslug}",
+author: "${author}",
+thumbnail: "${thumbnail}".
+description: "${BlocksToMarkdown(blurb)}"
+---
+
+${BlocksToMarkdown(body, { serializers, imageOptions, projectId, dataset })}
+`;
+    const filename = `posts/${date.split("T")[0]}_${mdslug}.md`;
+    fs.writeFile(filename, content, function (err) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log("The file was saved!", filename);
+    });
+  });
+}
+getDocs();
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Sanityio to Markdown exporter
+
+Updated from this original gist
+https://gist.github.com/kmelve/6955f890f832ff458ee2dbca36602b8f
